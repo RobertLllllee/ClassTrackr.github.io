de7baf84239513cba8cb168cfe050b505da93319
RobertLllllee <hongleejun@gmail.com>
2023-11-07 12:57:43 +0800

    Student dashboard showing name, ID and userype

--- script/student.js
+++ script/student.js
@@ -1,6 +1,22 @@
 console.log("student.js loaded");
 
 window.onload = function () {
+  fetchUserInfo();
+
+  // Function to fetch and display the user information
+  function fetchUserInfo() {
+    const userType = sessionStorage.getItem('userType');
+    const userId = sessionStorage.getItem('userId'); // Retrieve the appropriate user ID
+    const userName = sessionStorage.getItem('userName'); // Retrieve the appropriate user name
+
+    if (userType && userId) {
+      // Update the dashboard with UserType, UserID, and UserName
+      document.getElementById('userType').textContent = userType;
+      document.getElementById('userID').textContent = userId;
+      document.getElementById('userName').textContent = userName; // Display userName
+    }
+  };
+
   const userType = sessionStorage.getItem("userType");
   let userId = sessionStorage.getItem("userId");
 
