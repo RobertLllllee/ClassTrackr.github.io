a815d3a36f75bb7225441ec740d610d21bf938aa
Jack8236 <138234312+Jack8236@users.noreply.github.com>
2023-11-27 01:11:52 +0800

    Update checksubject.js

--- script/checksubject.js
+++ script/checksubject.js
@@ -1,3 +1,19 @@
+document.getElementById('sidebar-toggle-btn').addEventListener('click', function () {
+  var sidebar = document.querySelector('.sidebar');
+  var mainHeader = document.querySelector('.main-header');
+  var mainContent = document.querySelector('.main-content');
+
+  if (sidebar.classList.contains('sidebar-closed')) {
+    sidebar.classList.remove('sidebar-closed');
+    mainHeader.style.left = '255px';
+    mainContent.style.marginLeft = '250px';
+  } else {
+    sidebar.classList.add('sidebar-closed');
+    mainHeader.style.left = '0';
+    mainContent.style.marginLeft = '0';
+  }
+});
+
 document.addEventListener('DOMContentLoaded', async function () {
   try {
     // Make a GET request to fetch the current student's ID
